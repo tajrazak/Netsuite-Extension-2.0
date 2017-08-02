8965364580e75ef17e00f2d3d936b36d81055de5
Tajuddin Sayyad <Tajuddin@amzurtech.com>
2017-08-02 21:52:55 +0530

    taj:-removed the unneccessary files

--- Extension/content.js
+++
@@ -1,5 +0,0 @@
-if(window.location.href.search('https://debugger.na1.netsuite.com/app/common/scripting/scriptdebugger.nl?')>=0){
-	console.log(config)
-	document.getElementById('mainscript').value="require([],function(){\n\n})";
-}
-
